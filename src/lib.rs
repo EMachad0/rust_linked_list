555e7b1222fc8aad3e1d5f058c9dcfc4aceb6b5a
EMachad0 <elitonmachadod200@gmail.com>
2022-08-20 23:25:02 -0300

    documentation start

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,26 @@
+//! This crate aims to provide implementations in Rust of several data structures using linked lists as their underlying data structure
+//!
+//! This crate’s documentation provides some simple examples and extensively details the exposed API
+//!
+//! # Usage
+//! This crate is on [crates.io](https://crates.io/crates/rust_linked_list) and can be used by adding linked_lists_rs to your dependencies in your project’s Cargo.toml.
+//! ```toml
+//! [dependencies]
+//! linked_lists_rs = "1"
+//! ```
+//!
+//! # Example
+//! ```
+//! use linked_lists_rs::stack;
+//!
+//! fn main() {
+//!     let mut stack = stack::Stack::new();
+//! }
+//! ```
+
+/// Immutable List implementation
 pub mod immutable_list;
+/// Queue implementation
 pub mod queue;
+/// Stack implementation
 pub mod stack;
